d9e72d4d70c05b7e986b8ba17c8f7f5eafd83f55
Jyoti Sharma <jyoti.sharma@bbc.co.uk>
2021-04-07 19:05:57 +0100

    first commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,54 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const del = require('del');
-gulp.task('styles', () => {
-	return gulp.src('sass/**/*.scss')
-		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest('./css/'));
-});
-gulp.task('clean', () => {
-	return del([
-		'css/main.css',
-	]);
-});
-gulp.task('watch', () => {
-	gulp.watch('sass/**/*.scss', (done) => {
-		gulp.series(['clean', 'styles'])(done);
-	});
-});
-//gulp.task('default', gulp.series(['clean', 'styles']));
-gulp.task('default', gulp.series(['watch']));
+const gulp = require("gulp"),
+  sass = require("gulp-sass"),
+  sourcemaps = require("gulp-sourcemaps"),
+  browserSync = require("browser-sync").create(),
+  source = "./process/",
+  dest = "./builds/sassEssentials/";
+
+sass.compiler = require("node-sass");
+
+function html() {
+  return gulp.src(dest + "**/*.html");
+}
+
+function js() {
+  return gulp.src(dest + "**/*.js");
+}
+
+function styles() {
+  return gulp
+    .src(source + "sass/style.scss")
+    .pipe(sourcemaps.init())
+    .pipe(
+      sass({
+        sourcemap: true,
+        style: "compressed"
+      }).on("error", sass.logError)
+    )
+    .pipe(gulp.dest(dest + "css"));
+}
+
+function watch() {
+  gulp.watch(dest + "js/**/*.js", js).on("change", browserSync.reload);
+  gulp.watch(source + "sass/**/*", styles).on("change", browserSync.reload);
+  gulp.watch(dest + "index.html", html).on("change", browserSync.reload);
+}
+
+function server() {
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: dest
+    }
+  });
+
+  gulp
+    .watch(source + "sass/**/*.scss", styles)
+    .on("change", browserSync.reload);
+  gulp.watch(dest + "js/**/*.js", js).on("change", browserSync.reload);
+  gulp.watch(dest + "index.html", html).on("change", browserSync.reload);
+}
+
+var build = gulp.series(gulp.parallel(js, styles, html), server, watch);
+
+gulp.task("default", build);
